5e9af1d759c3006361ee6a20b2a66f91da943729
LudwinS <ludwinsaulromero@mail.com>
2026-03-17 15:05:45 -0600

    Configuracion para GitHub Pages

--- laboratorio2/vite.config.js
+++ laboratorio2/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Laboratorio-2-VUE/',
   plugins: [
     vue(),
     vueDevTools(),
